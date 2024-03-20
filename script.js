3103a180e6b66470d40f81a85369de9a7ee0bdd4
katumi10 <143261500+katumi10@users.noreply.github.com>
2024-03-20 08:53:20 -0700

    Javascript file

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+        let popup = document.getElementById("popup");
+
+        function showPopup(){
+            popup.classList.add("show-popup")
+
+        }
+        function closePopup(){
+            popup.classList.remove("show-popup")
+
+        }
